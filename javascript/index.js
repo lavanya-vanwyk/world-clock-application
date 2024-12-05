c1a6016d209ef4f507ac340af8b1be7bf049dca1
lavanya-vanwyk <lavanyavanwyk1@gmail.com>
2024-12-06 01:42:21 +0200

    updateCity function adding working dropdown
    updateCity retrieves real-time data for the city selected from the dropdown.
    
    Added user's local time.

--- javascript/index.js
+++ javascript/index.js
@@ -25,7 +25,46 @@ function updateTime() {
         "h:mm:ss [<small>]A[</small>]"
         );
     }
+    
+}
+
+function updateCity(event) {
+    
+    
+    let localTimeZone = moment.tz.guess();
+    let localTime = moment().tz(localTimeZone);
+    let localCity = localTimeZone.replace("_", " ").split("/")[1];
+    
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    
+    citiesElement.innerHTML = `
+        <div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+        "A"
+        )}</small></div>
+        </div>
+
+        <div class="city">
+        <div>
+        <h2>${localCity}</h2>
+        <div class="date">${localTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${localTime.format("h:mm:ss")} <small>${localTime.format(
+        "A"
+        )}</small></div>
+        </div>
+        `;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city-select");
+citiesSelectElement.addEventListener("change", updateCity);
